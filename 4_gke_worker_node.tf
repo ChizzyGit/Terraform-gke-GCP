6eebdcb3a65b93a34718c58c2e3ec048f89b8825
skydollstech <chizzyaugusta@gmail.com>
2025-01-19 23:47:24 +0100

    deploying gke to gcp

--- 4_gke_worker_node.tf
+++ 4_gke_worker_node.tf
@@ -1,11 +1,22 @@
 # Create Service Account
-#========================
 resource "google_service_account" "kubernetes" {
   account_id = "kubernetes"
 }
 
+# Define the GKE Cluster
+resource "google_container_cluster" "primary" {
+  name     = "primary-cluster"
+  location = "us-central1"
+  network  = "default"
+
+  # Define node locations (zones)
+  node_locations = ["us-central1-a", "us-central1-b", "us-central1-c"]
+
+  # Enable IP allocation
+  ip_allocation_policy {}
+}
+
 # Create worker node pool one
-#============================
 resource "google_container_node_pool" "general" {
   name       = "general"
   cluster    = google_container_cluster.primary.id
@@ -24,7 +35,7 @@ resource "google_container_node_pool" "general" {
   node_config {
     preemptible  = false
     machine_type = "e2-small"
-    disk_size_gb = 50 # Reduce to a value that fits your quota
+    disk_size_gb = 50
     labels = {
       role = "general"
     }
@@ -35,36 +46,3 @@ resource "google_container_node_pool" "general" {
     ]
   }
 }
-
-
-
-
-# # Create worker Node Pool two
-# resource "google_container_node_pool" "spot" {
-#   name    = "spot"
-#   cluster = google_container_cluster.primary.id
-
-#   management {
-#     auto_repair  = true
-#     auto_upgrade = true
-#   }
-
-#   autoscaling {
-#     min_node_count = 2
-#     max_node_count = 10
-#   }
-
-#   node_config {
-#     preemptible  = true
-#     machine_type = "e2-small"
-
-#     labels = {
-#       team = "devops"
-#     }
-
-#     service_account = google_service_account.kubernetes.email
-#     oauth_scopes = [
-#       "https://www.googleapis.com/auth/cloud-platform"
-#     ]
-#   }
-# }
